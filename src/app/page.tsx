60d06b0734cd66a8fb32ed3ee4a4ea53fec941cb
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-04-24 15:16:46 -0500

    Add dynmaic job loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import headshot from "../../public/headshot.jpg";
+import jobs from "./scratch/resume_jobs.json";
 
 export default function Home() {
   return (
@@ -14,62 +15,24 @@ export default function Home() {
         <h2 className="text-xl">Software Engineer</h2>
       </div>
       <div className="col-span-2">
-        <div className="h-[10rem] grid grid-cols-4 mx-5 mt-5 bg-[#222223] p-8 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]">
-          <div className="p-1 flex flex-col justify-center items-center">
-            <h2 className="text-2xl lg:text-4xl">8th Light</h2>
-            <p className="text-sm lg:text-md">
-              Sept 2021 -
-              <wbr />
-              &nbsp;Apr 2025
-            </p>
+        {jobs.map((job, index) => (
+          <div
+            key={index}
+            className="h-[11rem] grid grid-cols-4 mx-5 mt-3 bg-[#222223] p-2 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]"
+          >
+            <div className="flex flex-col justify-center items-center">
+              <h2 className="text-2xl lg:text-4xl">{job.company}</h2>
+              <p className="text-sm lg:text-md">{job.tenure}</p>
+            </div>
+
+            <div className="col-span-3 flex flex-col justify-center items-center border-l border-[rgb(61,61,62)]">
+              <h2 className="text-xl lg:text-3xl">{job.title}</h2>
+              <p className=" mt-2 mx-4 text-justify text-[13px] lg:text-sm">
+                {job.jobSummary}
+              </p>
+            </div>
           </div>
-          <div className="col-span-3 flex flex-col justify-center items-center border-l border-[rgb(61,61,62)]">
-            <h2 className="text-3xl">Job description</h2>
-            <p>Job Desccription</p>
-          </div>
-        </div>
-        <div className="h-[10rem] grid grid-cols-4 mx-5 mt-5 bg-[#222223] p-8 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]">
-          <div className="p-1 flex flex-col justify-center items-center">
-            <h2 className="text-2xl lg:text-4xl">8th Light</h2>
-            <p className="text-sm lg:text-md">
-              Sept 2021 -
-              <wbr />
-              &nbsp;Apr 2025
-            </p>
-          </div>
-          <div className="col-span-3 flex flex-col justify-center items-center border-l border-[rgb(61,61,62)]">
-            <h2 className="text-3xl">Job description</h2>
-            <p>Job Desccription</p>
-          </div>
-        </div>
-        <div className="h-[10rem] grid grid-cols-4 mx-5 mt-5 bg-[#222223] p-8 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]">
-          <div className="p-1 flex flex-col justify-center items-center">
-            <h2 className="text-2xl lg:text-4xl">8th Light</h2>
-            <p className="text-sm lg:text-md">
-              Sept 2021 -
-              <wbr />
-              &nbsp;Apr 2025
-            </p>
-          </div>
-          <div className="col-span-3 flex flex-col justify-center items-center border-l border-[rgb(61,61,62)]">
-            <h2 className="text-3xl">Job description</h2>
-            <p>Job Desccription</p>
-          </div>
-        </div>
-        <div className="h-[10rem] grid grid-cols-4 mx-5 my-5 bg-[#222223] p-8 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]">
-          <div className="p-1 flex flex-col justify-center items-center">
-            <h2 className="text-2xl lg:text-4xl">8th Light</h2>
-            <p className="text-sm lg:text-md">
-              Sept 2021 -
-              <wbr />
-              &nbsp;Apr 2025
-            </p>
-          </div>
-          <div className="col-span-3 flex flex-col justify-center items-center border-l border-[rgb(61,61,62)]">
-            <h2 className="text-3xl">Job description</h2>
-            <p>Job Desccription</p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
