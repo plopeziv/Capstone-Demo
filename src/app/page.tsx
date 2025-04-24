80e2da1084fe6d23e879217091969d340c0602af
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-04-24 10:08:08 -0500

    Create resume outline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,75 @@
 import Image from "next/image";
+import headshot from "../../public/headshot.jpg";
 
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center bg-[url('/background_images/soccer_pitch.jpg')] bg-cover bg-center">
-      <div className='w-[28rem] h-[20rem] mx-3 flex justify-center bg-gray-800 p-8 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]'>
-        <main className='flex justify-center flex-col gap-8 row-start-2 items-center sm:items-start'>
-          <div className='w-full flex justify-center'>
-            <Image
-              className='dark:invert'
-              src='/next.svg'
-              alt='Next.js logo'
-              width={180}
-              height={38}
-              priority
-            />
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-3 justify-center items-center bg-[#1E1E1F] bg-cover bg-center">
+      <div className="mx-5 mt-5 p-8 flex flex-col text-center justify-center items-center bg-[#222223] rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]">
+        <Image
+          className="mb-4 rounded-2xl"
+          src={headshot}
+          alt="profile picture"
+        />
+        <h1 className="text-4xl mb-1">Pedro Lopez</h1>
+        <h2 className="text-xl">Software Engineer</h2>
+      </div>
+      <div className="col-span-2">
+        <div className="h-[10rem] grid grid-cols-4 mx-5 mt-5 bg-[#222223] p-8 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]">
+          <div className="p-1 flex flex-col justify-center items-center">
+            <h2 className="text-2xl lg:text-4xl">8th Light</h2>
+            <p className="text-sm lg:text-md">
+              Sept 2021 -
+              <wbr />
+              &nbsp;Apr 2025
+            </p>
           </div>
-          <ol className='list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
-            <li className='mb-2'>
-              Get started by editing{" "}
-              <code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold'>
-                src/app/page.tsx
-              </code>
-              .
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className='flex gap-4 items-center flex-col sm:flex-row'>
-            <a
-              className='rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
-              href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <Image
-                className='dark:invert'
-                src='/vercel.svg'
-                alt='Vercel logomark'
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              className='rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44'
-              href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Read our docs
-            </a>
+          <div className="col-span-3 flex flex-col justify-center items-center border-l border-[rgb(61,61,62)]">
+            <h2 className="text-3xl">Job description</h2>
+            <p>Job Desccription</p>
           </div>
-        </main>
+        </div>
+        <div className="h-[10rem] grid grid-cols-4 mx-5 mt-5 bg-[#222223] p-8 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]">
+          <div className="p-1 flex flex-col justify-center items-center">
+            <h2 className="text-2xl lg:text-4xl">8th Light</h2>
+            <p className="text-sm lg:text-md">
+              Sept 2021 -
+              <wbr />
+              &nbsp;Apr 2025
+            </p>
+          </div>
+          <div className="col-span-3 flex flex-col justify-center items-center border-l border-[rgb(61,61,62)]">
+            <h2 className="text-3xl">Job description</h2>
+            <p>Job Desccription</p>
+          </div>
+        </div>
+        <div className="h-[10rem] grid grid-cols-4 mx-5 mt-5 bg-[#222223] p-8 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]">
+          <div className="p-1 flex flex-col justify-center items-center">
+            <h2 className="text-2xl lg:text-4xl">8th Light</h2>
+            <p className="text-sm lg:text-md">
+              Sept 2021 -
+              <wbr />
+              &nbsp;Apr 2025
+            </p>
+          </div>
+          <div className="col-span-3 flex flex-col justify-center items-center border-l border-[rgb(61,61,62)]">
+            <h2 className="text-3xl">Job description</h2>
+            <p>Job Desccription</p>
+          </div>
+        </div>
+        <div className="h-[10rem] grid grid-cols-4 mx-5 my-5 bg-[#222223] p-8 rounded-lg border border-gray-600 shadow-[inset_4px_4px_8px_rgba(255,255,255,0.1),_inset_-4px_-4px_8px_rgba(0,0,0,0.5)]">
+          <div className="p-1 flex flex-col justify-center items-center">
+            <h2 className="text-2xl lg:text-4xl">8th Light</h2>
+            <p className="text-sm lg:text-md">
+              Sept 2021 -
+              <wbr />
+              &nbsp;Apr 2025
+            </p>
+          </div>
+          <div className="col-span-3 flex flex-col justify-center items-center border-l border-[rgb(61,61,62)]">
+            <h2 className="text-3xl">Job description</h2>
+            <p>Job Desccription</p>
+          </div>
+        </div>
       </div>
     </div>
   );
