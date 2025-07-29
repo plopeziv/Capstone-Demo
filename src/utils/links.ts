5d3d7abe8bb2d4cdbd86fdf3f710c63029bbc550
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-07-29 11:07:29 -0500

    Remove NBA Link

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,6 +1,5 @@
 export const routingLinks = [
   { linkName: "Main Page", href: "/" },
-  { linkName: "NBA Dashboard", href: "/nba-dashboard" },
   { linkName: "NFL Dashboard", href: "/nfl-dashboard" },
   { linkName: "Sample Research", href: "/sample-research-dashboard" },
   { linkName: "Soccer Dashboard", href: "/soccer-dashboard" },
