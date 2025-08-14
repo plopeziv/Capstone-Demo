6c20f771cfc0c49965b79c8d635f7e4febbb5395
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-08-13 21:43:18 -0500

    Add hierarchical filtering

--- src/app/code-challenge/components/RestaurantContainer.tsx
+++ src/app/code-challenge/components/RestaurantContainer.tsx
@@ -12,6 +12,7 @@ export default function RestaurantContainer() {
 
   const activeFilters = useMunchiesStore((state) => state.filters);
 
+  // Fetches data from API
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
@@ -33,7 +34,9 @@ export default function RestaurantContainer() {
     fetchData();
   }, []);
 
+  // Filter logic that runs on filter or data change
   useEffect(() => {
+    // Early return for no filters active
     const areFiltersEmpty = Object.values(activeFilters).every(
       (filterGroup) => Array.isArray(filterGroup) && filterGroup.length === 0
     );
@@ -43,6 +46,7 @@ export default function RestaurantContainer() {
       return;
     }
 
+    // Helper function for upper and lower delivery time bounds
     const isDeliveryTimeInFilterScope = (restaurant) => {
       const deliveryTimeFilters = activeFilters.deliveryTime;
       const restaurantDeliveryTime = restaurant.delivery_time_minutes;
@@ -57,10 +61,26 @@ export default function RestaurantContainer() {
       });
     };
 
-    const filteredData = originalData.filter((restaurant) => {
-      const isInCuisineFilter = restaurant.filter_ids.some((filterId) =>
+    // sets restaurants to be used for hierarchical filtering
+    const availableRestaurants = originalData.filter((restaurant) => {
+      if (activeFilters.cuisineFilters.length === 0) {
+        return true;
+      }
+
+      return restaurant.filter_ids.some((filterId) =>
         activeFilters.cuisineFilters.includes(filterId)
       );
+    });
+
+    // Filters based on time and price for available restaurants
+    const filteredData = availableRestaurants.filter((restaurant) => {
+      // Early return for empty filters
+      if (
+        activeFilters.price.length === 0 &&
+        activeFilters.deliveryTime.length === 0
+      ) {
+        return true;
+      }
 
       const isInPriceFilter = activeFilters.price.includes(
         restaurant.price_range_id
@@ -68,7 +88,7 @@ export default function RestaurantContainer() {
 
       const isInTimeThreshold = isDeliveryTimeInFilterScope(restaurant);
 
-      return isInCuisineFilter || isInPriceFilter || isInTimeThreshold;
+      return isInPriceFilter || isInTimeThreshold;
     });
 
     setRestaurants(filteredData);
@@ -86,11 +106,17 @@ export default function RestaurantContainer() {
 
   return (
     <div className="rounded-md flex-1">
-      <div className="flex flex-wrap justify-start gap-2">
-        {restaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.id} restaurantData={restaurant} />
-        ))}
-      </div>
+      {restaurants.length > 0 ? (
+        <div className="flex flex-wrap justify-start gap-2">
+          {restaurants.map((restaurant) => (
+            <RestaurantCard key={restaurant.id} restaurantData={restaurant} />
+          ))}
+        </div>
+      ) : (
+        <div className=" h-full flex flex-col justify-center text-center">
+          <span className="text-4xl mt-3">No Restaurants Found!</span>
+        </div>
+      )}
     </div>
   );
 }
