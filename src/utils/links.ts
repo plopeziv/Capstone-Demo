8826bb694b37c44487a1ec0db6ad27139ffff045
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-08-16 12:10:50 -0500

    hotfix: rearrange link order

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,7 +1,7 @@
 export const routingLinks = [
   { linkName: "Main Page", href: "/" },
-  { linkName: "NFL Dashboard", href: "/nfl-dashboard" },
-  { linkName: "Coding Challenge", href: "/code-challenge" },
   { linkName: "Sample Research", href: "/sample-research-dashboard" },
+  { linkName: "Coding Challenge", href: "/code-challenge" },
   { linkName: "Soccer Dashboard", href: "/soccer-dashboard" },
+  { linkName: "NFL Dashboard", href: "/nfl-dashboard" },
 ];
